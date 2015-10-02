993ea0aa28129b9f5dbc1c0c8abd7ed182cb006e
brunodantas <bruno.welldm@gmail.com>
2015-10-02 09:22:01 -0300

    aplicação no lado do servidor - não testado

--- app-server.h
+++ app-server.h
@@ -3,11 +3,14 @@
 #include "lista.h"
 
 
-//variável global da aplicação
-Lista listatelefonica;
+//variáveis globais
+Lista listatelefonica;	//lista usada pela aplicação
+Registro* reg_atual;	//ponteiro auxiliar para as funcionalidades (inserir,remover etc)
+int func_atual;		//funcionalidade a ser executada
 
 
 //registro a ser inserido na lista telefônica
+//nome é a chave
 struct registro
 {
 	char nome[55];	//chave do registro
@@ -16,20 +19,25 @@ struct registro
 typedef struct registro Registro;
 
 
+//recebe uma mensagem e seta func_atual, reg_atual de acordo com o protocolo da aplicação
+//func_atual = -1 se há falha no protocolo
+void processa_msg(char *msg);
+
+
 //insere registro válido na lista sem garantia de que não há repetição
 int inserir(Registro r);
 
 
 //altera o telefone de um registro e o retorna
-Registro alterar(char chave[55]);
+Registro alterar(char* chave);
 
 
 //remove registro da lista e retorna sucesso ou fracasso se não encontrado
-int remover(char chave[55]);
+int remover(char* chave);
 
 
 //busca pelo nome
-Registro buscar(char chave[55]);
+Registro buscar(char* chave);
 
 
 //encerra aplicação
